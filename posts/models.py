18c387d77823d4228f980c1c419014e6e6e1e47d
jod431047 <jod431047@gmail.com>
2023-06-20 18:40:49 +0200

    add tags library

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
 
 # Create your models here. python manage.py makemigrations . python manage.py migrate, python manage.py runserver
 class Author(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField(max_length=500)
+    image = models.ImageField(upload_to='auhtors')
     def __str__(self):
         return self.name
     
@@ -16,5 +18,7 @@ class post (models.Model):
     publish_date = models.DateTimeField()
     content = models.TextField(max_length=15000)
     Author = models.ForeignKey(Author,related_name='post_author',on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='posts')
+    tags = TaggableManager()
     def __str__(self):
         return self.title
--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'posts',                #das ist unsre app
+   
+    'posts',  
+    'taggit',                                 #das ist unsre app
 ]
 
 MIDDLEWARE = [
